b37c4f68adb1913629397eb0533dbbbbbebfdffe
SuperSkyDeveloper <super.skystar92@gmail.com>
2021-11-15 03:54:40 +0800

    update url

--- src/url.js
+++ src/url.js
@@ -1,6 +1,6 @@
 export const url = {
-    "local": 'http://localhost:8000/graphql',
-    // "local":'http://resellerpanel.info:3000/graphql',
+    // "local": 'http://localhost:8000/graphql',
+    "local":'http://resellerpanel.info:3000/graphql',
     // "local": 'http://54.173.5.83/graphql',
 
 }
