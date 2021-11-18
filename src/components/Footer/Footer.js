f17f9371c89849c1f1ea0c458da4763b7b20b1b3
SuperSkyDeveloper <super.skystar92@gmail.com>
2021-11-18 23:24:24 +0800

    update logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css';
 const Footer = props =>(
     <footer className="footer">
-        <p>Copyright@2021 Activation Panel</p>
+        <p>Copyright@2021 Activation Panel </p>
     </footer>
 );
   
