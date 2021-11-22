7a9490869f4ed81a048b898f070ca21f50e0d034
SuperSkyDeveloper <super.skystar92@gmail.com>
2021-11-22 22:01:23 +0800

    update variables

--- src/data.js
+++ src/data.js
@@ -25,12 +25,12 @@ export const Data = {
         },
     ],
     alterData: {
-        using: {
+        other: {
             adminId:"6195c3427f82ab43b9e571ed",
             url: 'http://localhost:8000/graphql',
             payment:"sandbox",
         },
-        other:{
+        using:{
             adminId:"616c5a56d4b61b864bfc72c7",
             url:'https://resellerpanel.info/api',
             payment:"production",
